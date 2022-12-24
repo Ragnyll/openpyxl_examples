40397428481a944b56c61d45149f5714c70189dc
ragnyll <ragnyll@gallowzhumour.dev>
2022-12-24 14:40:59 -0600

    wrote a bond function

--- element.py
+++ element.py
@@ -11,16 +11,37 @@ class Element:
         return
 
     def calculate_gforce_weight(self):
-        # calculating things are hard
-        # call some subroutine to make it a little easier
+        """calculating things are hard
+
+        call some subroutine to make it a little easier
+        """
         a_multiplier = multiply_avocados(2)
         return self.atomic_weight * a_multiplier
 
+    def bond(self, other):
+        """Tries to bond this element with other
+        """
+        if self.name == other.name:
+            print('The bond occured')
+        else:
+            raise BondException('element cant bond')
+
+        return
+
     def write_to_sheet(self, sheet):
-        sheet.title = '{} Rocket Stats'.format(self.name)
+        sheet.title = '{} Element Stats'.format(self.name)
         cell_range = sheet['A1': 'A3']
         cell_range[0][0].value = self.name
         cell_range[1][0].value = self.atomic_weight
         cell_range[2][0].value = self.periodic_number
 
         return
+
+
+class BondException(Exception):
+    """A failure occured when trying to bond 2 elements"""
+    def __init__(self, *args):
+        super().__init__(args)
+
+    def __str__(self):
+        return 'The elements could not bond for some reason'
