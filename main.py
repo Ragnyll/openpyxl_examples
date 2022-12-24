ddc085433e3db8e3341aab58873fea8eaa9085fe
ragnyll <ragnyll@gallowzhumour.dev>
2022-12-24 14:32:22 -0600

    generate a datasheet we can use here

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import sys
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from element import Element
+import random
 
 
 def generate_square_data(sheet):
     counter = 0
     for row in range(2, 10):
-        for col in range(2,10):
+        skipped_cell = False
+        for col in range(2, 6):
+            if not skipped_cell and random.randrange(0, 4, 1) == 0:
+                skipped_cell = True
+                continue
             cell = sheet.cell(column=col, row=row, value="{0}".format(get_column_letter(col)))
             cell.value = counter
             counter += 1
